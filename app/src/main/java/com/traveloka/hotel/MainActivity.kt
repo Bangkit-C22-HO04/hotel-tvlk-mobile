86369c1e4f6b026e5f27a42b6c44d98918fc27da
althof3 <althoframdhan3@gmail.com>
2022-05-29 14:32:20 +0700

    add minimal UI

--- app/src/main/java/com/traveloka/hotel/MainActivity.kt
+++ app/src/main/java/com/traveloka/hotel/MainActivity.kt
@@ -3,13 +3,6 @@ package com.traveloka.hotel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.traveloka.hotel.navigation.HotelNavigation
 import com.traveloka.hotel.ui.theme.HotelmobileTheme
 
