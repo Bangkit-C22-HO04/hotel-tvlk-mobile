6c37afb5b4c42b416647af15ac2a79e9a5f780bb
Syabib Ash-Shiddiqi <sasfort14@gmail.com>
2022-06-01 23:55:12 +0700

    create review model and dummy data for reviews

--- app/src/main/java/com/traveloka/hotel/utils/Constants.kt
+++ app/src/main/java/com/traveloka/hotel/utils/Constants.kt
@@ -2,7 +2,9 @@ package com.traveloka.hotel.utils
 
 import com.traveloka.hotel.R
 import com.traveloka.hotel.model.Hotel
+import com.traveloka.hotel.model.Review
 import org.json.JSONArray
+import java.text.SimpleDateFormat
 
 val TRAVEL_PURPOSE_OPTIONS = listOf(
     R.string.culture_sightseeing,
@@ -40,6 +42,51 @@ fun getHotels(): List<Hotel> {
 
     return hotels
 }
+
+fun getReviews(): List<Review> {
+
+    val reviews = mutableListOf<Review>()
+    val reviewsJson = JSONArray(reviewJsonArray)
+    val formatter = SimpleDateFormat("dd-MM-yyyy")
+
+    for (i in 0 until reviewsJson.length()) {
+
+        val review = reviewsJson.getJSONObject(i)
+        val reviewModel = Review(
+            id = review["id"].toString(),
+            name = review["name"].toString(),
+            rating = review["rating"].toString().toDouble(),
+            date = formatter.parse(review["date"].toString()),
+            tag = review["tag"].toString(),
+            content = review["content"].toString()
+        )
+
+        reviews.add(reviewModel)
+    }
+
+    return reviews
+}
+
+const val reviewJsonArray = """
+    [
+        {
+            "id": "1",
+            "name": "Bangkit 2022",
+            "rating": 7.3,
+            "date": "2019-04-16 12:18:06.018950",
+            "tag": "One-night stay",
+            "content": "A really great hotel!"
+        },
+        {
+            "id": "2",
+            "name": "Bangkit 2023",
+            "rating": 8.9,
+            "date": "2020-04-16 12:18:06.018950",
+            "tag": "Family vacation",
+            "content": "A really great hotel!"
+        },
+    ]
+"""
 
 const val hotelJsonArray = """
   [
