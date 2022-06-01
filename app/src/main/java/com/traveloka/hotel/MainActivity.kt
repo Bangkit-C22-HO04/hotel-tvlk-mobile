76a0719f48a23b8528b2c177072c3d4a119c972a
althof3 <althoframdhan3@gmail.com>
2022-06-01 15:28:01 +0700

    change status bar color

--- app/src/main/java/com/traveloka/hotel/MainActivity.kt
+++ app/src/main/java/com/traveloka/hotel/MainActivity.kt
@@ -3,7 +3,10 @@ package com.traveloka.hotel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.SideEffect
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.traveloka.hotel.ui.navigation.HotelNavigation
+import com.traveloka.hotel.ui.theme.Blue
 import com.traveloka.hotel.ui.theme.HotelmobileTheme
 
 class MainActivity : ComponentActivity() {
@@ -12,6 +15,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
+            val systemUiController = rememberSystemUiController()
+            SideEffect {
+                systemUiController.setStatusBarColor(Blue)
+            }
             HotelmobileTheme {
                 HotelNavigation()
             }
